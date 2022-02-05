6c59ade5017c57fb0f7145495caccf58b765fa38
Maor Bane <skf.maor@gmail.com>
2022-02-05 13:54:08 +0200

    support attributes expressions

--- test/ssr.test.js
+++ test/ssr.test.js
@@ -8,32 +8,32 @@ var Var = {
 
 global.Var = Var
 
-test('div', async () => {
-  const input = `export const App = () => <div style="color: red">test</div>`
-  const output = parser(input)
-  const file = 'dist/ssr-div.js'
-  
+async function testJsx(input, output, file, expectation) {
   fs.writeFileSync(file, output, 'utf8')
   
   const { App } = (await import('../' + file))
   const result = ssr(App())
   
-  expect(result).toEqual('<div style="color: red">test</div>')
+  expect(result).toEqual(expectation)
   fs.rmSync(file)
+}
+
+test('div', async () => {
+  const input = `export const App = () => <div style="color: red" class={'test'}>test</div>`
+  const output = parser(input)
+  const file = 'dist/ssr-div.js'
+  const expectation = `<div style="color: red" class="test">test</div>`
+  
+  return testJsx(input, output, file, expectation)
 })
 
 test('hr', async () => {
   const input = `export const App = () => <hr />`
   const output = parser(input)
   const file = 'dist/ssr-hr.js'
+  const expectation = `<hr />`
   
-  fs.writeFileSync(file, output, 'utf8')
-  
-  const { App } = (await import('../' + file))
-  const result = ssr(App())
-  
-  expect(result).toEqual('<hr />')
-  fs.rmSync(file)
+  return testJsx(input, output, file, expectation)
 })
 
 test('component', async () => {
@@ -43,11 +43,7 @@ test('component', async () => {
   `
   const output = parser(input)
   const file = 'dist/ssr-component.js'
+  const expectation = `<div style="color: red"></div>`
   
-  fs.writeFileSync(file, output, 'utf8')
-  
-  const { App } = (await import('../' + file))
-  const result = ssr(App())
-  expect(result).toEqual('<div style="color: red"></div>')
-  fs.rmSync(file)
+  return testJsx(input, output, file, expectation)
 })
